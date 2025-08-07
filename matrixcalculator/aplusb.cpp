7cc3460f38a3dcfdfaf3c264852af3a170c43927
leoapply092063 <leoapply092063@gmail.com>
2025-08-07 15:43:32 -0300

    Update aplusb.cpp

--- matrixcalculator/aplusb.cpp
+++ matrixcalculator/aplusb.cpp
@@ -6,7 +6,7 @@
 
 extern "C" {
     EMSCRIPTEN_KEEPALIVE
-    void processMatrix(int* matrixData, int size, int cols) {
+    void processMatrix(int* matrixData, int size,int rows int cols) {
         int mats = size / (rows * cols);
 
         int* resultMatrix = new int[rows * cols];
@@ -67,6 +67,7 @@ extern "C" {
 }
 
  
+
 
 
 
